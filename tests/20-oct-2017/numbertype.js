c2c64844bc69ef6f262419677ea85274690ca725
montshejohn <mokgokongjohannes@gamil.com>
2017-10-20 13:20:10 +0200

    added conditional statements

--- tests/20-oct-2017/numbertype.js
+++ tests/20-oct-2017/numbertype.js
@@ -1,13 +1,17 @@
 function checkNumbers(numbers) {
     numbers = Array.from(arguments);
     for (var i = 0; i < numbers.length; i++) {
-        for (var b = 0; b > numbers.length; i--) {
-            console.log(numbers[b]);
-            console.log(numbers[i]);
+        //console.log(typeof numbers[i] == typeof 1);
+        // console.log(type);
+        if (typeof numbers[i] == typeof 1) {
+            return false;
+        } else {
+            // console.log(typeof numbers[i]);
+            return true;
         }
 
     }
-
-}(checkNumbers(1, "a", 1, 4, 5, 1));
+}
+console.log(checkNumbers(1, "a", 1, 4, 5, 1));
 //console.log(checkNumbers(2, "NaN", 1, 4, 5, 1));
-(checkNumbers(1, 3, 1, 4, 5, 1));
+console.log(checkNumbers(1, 3, 1, 4, 5, 1));
