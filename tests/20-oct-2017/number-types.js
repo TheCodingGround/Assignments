64ec308a2f2f168a2020bf1e79321e708a0a272d
montshejohn <mokgokongjohannes@gamil.com>
2017-10-20 13:37:29 +0200

    renamed file names

--- tests/20-oct-2017/number-types.js
+++ tests/20-oct-2017/number-types.js
@@ -1,7 +1,7 @@
 function checkNumbers(numbers) {
     numbers = Array.from(arguments);
     for (var i = 0; i < numbers.length; i++) {
-        //console.log(typeof numbers[i] == typeof 1);
+        console.log(typeof numbers[i] == typeof 1);
         // console.log(type);
         if (typeof numbers[i] == typeof 1) {
             return false;
@@ -14,4 +14,4 @@ function checkNumbers(numbers) {
 }
 console.log(checkNumbers(1, "a", 1, 4, 5, 1));
 //console.log(checkNumbers(2, "NaN", 1, 4, 5, 1));
-console.log(checkNumbers(1, 3, 1, 4, 5, 1));
+//console.log(checkNumbers(1, 3, 1, 4, 5, 1));
