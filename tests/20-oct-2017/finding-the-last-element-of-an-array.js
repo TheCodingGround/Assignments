4c74ab05e11c2acc204c345e8d7eba6130e46c36
montshejohn <mokgokongjohannes@gamil.com>
2017-10-23 10:08:43 +0200

    fixed errors when replacing last index of a string

--- tests/20-oct-2017/finding-the-last-element-of-an-array.js
+++ tests/20-oct-2017/finding-the-last-element-of-an-array.js
@@ -1,9 +1,15 @@
 function getLastIndex(list) {
     list = Array.from(arguments);
-    console.log(list);
-    var theLastindex = list.slice(-1);
-    return theLastindex;
+    var result = "";
+    var lastIndex = list.length - 1;
+    result = list[lastIndex];
+    if (list.length === 1) {
+        var arrIndex = list[lastIndex].length - 1;
+        result = list[lastIndex][arrIndex];
+    }
+    return result;
 }
-console.log(getLastIndex(1, 2, 3, 4, 5));
-console.log(getLastIndex(4, 3, 2, 1, -1));
+
+console.log(getLastIndex("xyz"));
+console.log(getLastIndex([4, 3, 2, 1, -1]));
 console.log(getLastIndex(12, 11, 12, 991, ));
