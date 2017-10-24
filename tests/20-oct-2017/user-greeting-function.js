18830f082f1e0d72f8f8ea172da323c50dc91224
montshejohn <mokgokongjohannes@gamil.com>
2017-10-24 13:37:51 +0200

    re-done user-greeting js file

--- tests/20-oct-2017/user-greeting-function.js
+++ tests/20-oct-2017/user-greeting-function.js
@@ -1,5 +1,13 @@
 function person(name, otherName) {
-
-    return `Hi ${otherName} my name is ${name}`;
+    this.name = name;
+    this.otherName = otherName;
 }
-console.log(person("johannes", "mokgokong"));
+person.prototype.greet = function() {
+    return "Hi " + this.otherName + ", my name is " + this.name;
+}
+var greets = new person("johannes", "mokgokong");
+var person1 = new person("person1", "person2");
+var person3 = new person("person3", "persone3");
+console.log(greets.greet());
+console.log(person1.greet());
+console.log(person3.greet());
