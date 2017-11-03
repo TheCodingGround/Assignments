ac5cc5053789995e6a5a246b2686543dc6df7702
montshejohn <mokgokongjohannes@gamil.com>
2017-11-03 10:09:21 +0200

    added readme file

--- tests/26-oct-2017/smaller-numbers-in-an-array.js
+++ tests/26-oct-2017/smaller-numbers-in-an-array.js
@@ -1,4 +1,4 @@
-function getSmallerelements(arr, givenNum) {
+/*function getSmallerElements(arr, givenNum) {
     var smallerNumbers = [];
     for (var n = 0; n < arr.length; n++) {
         if (arr[n] < givenNum) {
@@ -7,6 +7,16 @@ function getSmallerelements(arr, givenNum) {
     }
     return smallerNumbers;
 }
-console.log(getSmallerelements([1, 3, 4, 5, 6, 7, 9, 12, 29, 14], 10));
-console.log(getSmallerelements([1.3, 3.3, 4, 5, 6, 7.3, 9, 12, 29, 1.4], 5));
-console.log(getSmallerelements([1, 3, 4, 5, 6, 7, 9, 12, 2.9, 144], 3.0));
+console.log(getSmallerElements([1, 3, 4, 5, 6, 7, 9, 12, 29, 14], 10));
+console.log(getSmallerElements([1.3, 3.3, 4, 5, 6, 7.3, 9, 12, 29, 1.4], 5));
+console.log(getSmallerElements([1, 3, 4, 5, 6, 7, 9, 12, 2.9, 144], 3.0));
+*/
+
+
+var list = [1, 2, 3, 4, 4, 5, 510]
+var thild = list.filter(function(num) {
+    return num < 10;
+})
+return thild;
+
+//console.log(array);
