3c3f681fe3372fbddafe9d1e03a0c0cbe9b51f91
montshejohn <mokgokongjohannes@gamil.com>
2017-12-01 11:14:34 +0200

    added greater than or equal checker

---
+++ tests/2017-12-01/greater-than-or-equal-check.js
@@ -0,0 +1,9 @@
+// Write a function that takes 3 arguments, an array of numbers, a number
+//  und a string that will either be "GreaterOrEqual" or "LessThan"
+//   and return an array of numbers where the numbers are Greater Or 
+//   Equal if the third argument is "GreaterOrEqual", or less than if 
+//   the third argument is "LessThan". Give an appropriate name for the 
+//   function.
+// This function should use a for loop.
+// ExampleFunction([1,2,3,4,5,6,7], 3, "GreaterOrEqual") = [3,4,5,6,7]
+// ExampleFunction([1,2,3,4,5,6,7], 5, "LessThan") = [1,2,3,4]
