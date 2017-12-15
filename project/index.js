1139576eef694ba2a08bfc911644555a36561c3d
montshejohn <mokgokongjohannes@gamil.com>
2017-12-15 16:09:31 +0200

    added twitter oauth where user can verify

--- project/index.js
+++ project/index.js
@@ -1,66 +1,54 @@
-var express = require("express");
-var app = express();
-var request = require("request");
-var querryString = require("querystring");
-var qs = require("querystring");
+const express = require("express");
+const app = express();
+const qs = require("qs");
+const request = require("request");
+
 var oauth = {
-  callback: "http://localhost:3000/twitter/callback",
-  consumer_key: "Rz63spEaepbrHThkMtr5TJgFj",
-  consumer_secret: "1hir1CnQKcH5Ma27EvorqqvRI8vi5F1lgO3jbsTVgM70qB5YII"
-};
-
-var url = "https://api.twitter.com/oauth/request_token";
-
-// request.post({ url: url, oauth: oauth }, function(e, r, body) {
-//   console.log(body);
-//   var req_data = qs.parse(body);
-//   var uri =
-//     "https://api.twitter.com/oauth/authenticate" +
-//     "?" +
-//     qs.stringify({ oauth_token: req_data.oauth_token });
-//  var auth_data  = qs.parse(body),
-function findAccesToken(verifier, request, cb) {
-  var oauthDetails = {
+    callback: "http://localhost:3000/twitter/callback",
     consumer_key: "Rz63spEaepbrHThkMtr5TJgFj",
-    consumer_secret: "1hir1CnQKcH5Ma27EvorqqvRI8vi5F1lgO3jbsTVgM70qB5YII",
-    token: reqToken
-    // token_secret: req_data.oauth_token_secret,
-    //verifier: auth_data.oauth_verifier
-  };
-  request.post(
-    { url: "https://api.twitter.com/oauth/access_token", oauth: oauthDetails },
-    function(e, r, body) {
-      console.log("body=", body);
-      if(e){console.log()}
-    }
-  );
-}
-//       oauth = {
-//         consumer_key: CONSUMER_KEY,
-//         consumer_secret: CONSUMER_SECRET,
-//         token: perm_data.oauth_token,
-//         token_secret: perm_data.oauth_token_secret
-//       },
-//       url = "https://api.twitter.com/1.1/users/show.json",
-//       qs = {
-//         screen_name: perm_data.screen_name,
-//         user_id: perm_data.user_id
-//       };
-//     request.get({ url: url, oauth: oauth, qs: qs, json: true }, function(
-//       e,
-//       r,
-//       user
-//     ) {
-//       console.log(user);
-//     });
-//   });
-// });
-app.get("/", function(req, res) {
-  res.send(
-    "<a href= 'https://api.twitter.com/oauth/authenticate'" +
-      "?" +
-      ">click here to verify</a>"
-  );
+    consumer_secret: "1hir1CnQKcH5Ma27EvorqqvRI8vi5F1lgO3jbsTVgM70qB5YII"
+  },
+  url = "https://api.twitter.com/oauth/request_token";
+request.post({ url: url, oauth: oauth }, function(e, r, body) {
+  console.log("body = ", body);
+
+  app.get("/authorize/twitter", (req, res) => {
+    console.log("Requesting a refresh token from twitter");
+    var req_data = qs.parse(body);
+    console.log(req_data);
+    res.send(
+      "<a href='https://api.twitter.com/oauth/authorize?oauth_token=" +
+        req_data.oauth_token +
+        "'>Authorize Twitter</a>"
+      );
+      var  oauth =
+      { consumer_key: "Rz63spEaepbrHThkMtr5TJgFj"
+      , consumer_secret:"1hir1CnQKcH5Ma27EvorqqvRI8vi5F1lgO3jbsTVgM70qB5YII"
+      , token: auth_data.oauth_token
+      , token_secret: req_data.oauth_token_secret
+      , verifier: auth_data.oauth_verifier
+      }
+    app.listen(3000, () =>
+      console.log("The server started correctly and is listening on port 3000!")
+    );
+  });
+
+  // var uri =
+  //   "https://api.twitter.com/oauth/authenticate" +
+  //   "?" +
+  //   qs.stringify({ oauth_token: req_data.oauth_token });
 });
 
-app.listen(3000, () => console.log("server running at port 3000"));
+function getRequestToken(cb) {
+  var oauth = {
+    callback: "http://localhost:3000/twitter/callback",
+    consumer_key: "Rz63spEaepbrHThkMtr5TJgFj",
+    consumer_secret: "1hir1CnQKcH5Ma27EvorqqvRI8vi5F1lgO3jbsTVgM70qB5YII"
+  };
+}
+
+app.get("/", (req, res) => res.send("<h1>Hello World!</h1>"));
+
+app.listen(3000, () =>
+  console.log("The server started correctly and is listening on port 3000!")
+);
