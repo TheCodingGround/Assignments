a16d04791334be60026290cb908eadf6506710bf
montshejohn <mokgokongjohannes@gmail.com>
2018-04-02 10:23:50 +0200

    Fixed bug with not displaying currenctcalc correctly when pressing equals

--- React/Calculator/calculator/src/App.js
+++ React/Calculator/calculator/src/App.js
@@ -1,78 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Buttons from './Buttons';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Buttons from "./Buttons";
 
 class App extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-          currentCalc: '',
-            currentTotal: 0,
-            screenValue: "",
-            name: '',
-            surname: ''
-        };
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      currentCalc: "",
+      currentTotal: 0,
+      screenValue: "",
+      name: "",
+      surname: ""
+    };
 
-        this.addToScreenValue = this.addToScreenValue.bind(this);
-        this.setInput = this.setInput.bind(this);
-    }
+    this.addToScreenValue = this.addToScreenValue.bind(this);
+    this.setInput = this.setInput.bind(this);
+  }
 
-    plus(){
-      var newState = this.state;
-      newState.currentCalc = newState.currentCalc + Number(this.state.screenValue).toString() + "+";
-      this.setState({...newState})
-      this.clear()
-    }
+  plus() {
+    var newState = this.state;
+    newState.currentCalc =
+      newState.currentCalc + Number(this.state.screenValue).toString() + "+";
+    this.setState({ ...newState });
+    this.clear();
+  }
 
-    equals(){
-      var currentCalc = this.state.currentCalc + this.state.screenValue;
-      var calcResult = eval(currentCalc);
+  equals() {
+    var currentCalc = this.state.currentCalc + this.state.screenValue;
+    var calcResult = eval(currentCalc);
 
-      this.setState({
-        screenValue: calcResult, 
-        currenctCalc:currentCalc
-      })
+    this.setState({
+      screenValue: calcResult,
+      currentCalc: currentCalc
+    });
+  }
 
+  addToScreenValue(event) {
+    event.preventDefault();
 
-    }
+    var newState = this.state;
+    newState.screenValue += event.target.name;
+    this.setState(newState);
+  }
 
-    addToScreenValue(event){
-        event.preventDefault();
+  setInput(e) {
+    var state = this.state;
 
-        var newState = this.state;
-        newState.screenValue += event.target.name;
-        this.setState(newState);
-    }
+    state[e.target.name] = e.target.value;
 
-    setInput(e){
-        var state = this.state;
+    this.setState({ ...state });
+  }
 
-        state[e.target.name] = e.target.value;
-
-        this.setState({...state});
-    }
-
-    clear(){
-      this.setState({screenValue:''});
-    }
+  clear() {
+    this.setState({ screenValue: "" });
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to our Calculator. Happy Calculating</h1>
+          <h1 className="App-title">
+            Welcome to our Calculator. Happy Calculating
+          </h1>
         </header>
-        <Buttons clear={() => this.clear()} 
-        currentCalc={this.state.currentCalc}
-        addToScreenValue={this.addToScreenValue} 
-        screenValue={this.state.screenValue}
-        plus={() => this.plus()}
-        equals={()=> this.equals()}
-        
+        <Buttons
+          clear={() => this.clear()}
+          currentCalc={this.state.currentCalc}
+          addToScreenValue={this.addToScreenValue}
+          screenValue={this.state.screenValue}
+          plus={() => this.plus()}
+          equals={() => this.equals()}
         />
-        
       </div>
     );
   }
