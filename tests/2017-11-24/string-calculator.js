1daab2048dd5e937f08bdae4a1c5d2137bbd2c9c
montshejohn <mokgokongjohannes@gmail.com>
2018-06-04 14:59:02 +0200

    refactored code

--- tests/2017-11-24/string-calculator.js
+++ tests/2017-11-24/string-calculator.js
@@ -1,21 +1,17 @@
-const StringCal = numbers => {
+const stringCal = numbers => {
   if (numbers.indexOf("-") !== -1 && numbers[0] !== "/") {
     throw new Error(`negatives not allowed`);
   }
-  let yu = numbers.split(/\W/);
-  let arr = [];
-  for (let i = 0; i < yu.length; i++) {
-    arr.push(parseInt(yu[i]));
+  let omitNonDigit = numbers.split(/\W/);
+  let addToArray = [];
+  for (let i = 0; i < omitNonDigit.length; i++) {
+    addToArray.push(+(omitNonDigit[i]));
   }
-  let filled = arr.filter(x => x >= 0);
-  let maped = filled.reduce((a, b) => a + b, 0);
+  let returnNumbersGreater = addToArray.filter(x => x >= 0);
+  let maped = returnNumbersGreater.reduce((a, b) => a + b, 0);
   return maped;
 };
 
-console.log(StringCal(""));
-console.log(StringCal("1"));
-console.log(StringCal("1,23,2,2"));
-console.log(StringCal("1,\n"));
-console.log(StringCal("//;1;2"));
-console.log(StringCal("//-\n-23-2-2"));
-console.log(StringCal("1,-2"));
+module.exports = {
+  stringCal
+}
