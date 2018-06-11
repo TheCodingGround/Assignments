6da26ba2dc5fc254308ae77af8f481c7b2e2e7a2
montshejohn <mokgokongjohannes@gmail.com>
2018-06-11 11:17:45 +0200

    refactor string calculator

--- tests/2017-11-24/string-calculator.js
+++ tests/2017-11-24/string-calculator.js
@@ -1,17 +1,28 @@
-const stringCal = numbers => {
-  if (numbers.indexOf("-") !== -1 && numbers[0] !== "/") {
-    throw new Error(`negatives not allowed`);
-  }
-  let omitNonDigit = numbers.split(/\W/);
-  let addToArray = [];
-  for (let i = 0; i < omitNonDigit.length; i++) {
-    addToArray.push(+(omitNonDigit[i]));
-  }
-  let returnNumbersGreater = addToArray.filter(x => x >= 0);
-  let maped = returnNumbersGreater.reduce((a, b) => a + b, 0);
-  return maped;
-};
+// const stringCal = numbers => {
+//   if (numbers.indexOf("-") !== -1 && numbers[0] !== "/") {
+//     throw new Error(`negatives not allowed`);
+//   }
+//   let omitNonDigit = numbers.split(/\W/);
+//   let addToArray = [];
+//   for (let i = 0; i < omitNonDigit.length; i++) {
+//     addToArray.push(+(omitNonDigit[i]));
+//   }
+//   let returnNumbersGreater = addToArray.filter(x => x >= 0);
+//   let maped = returnNumbersGreater.reduce((a, b) => a + b, 0);
+//   return maped;
+// };
 
+function stringCal(numbers) {
+  var n = numbers.match(/[-]/);
+  if (n !== null) {
+    
+    throw new Error("negatives not allowed");
+    return;
+  }
+  const splitArgs = numbers.split(/\W/g);
+  const reducedVals = splitArgs.reduce((acc, val) => +acc + +val);
+  return +reducedVals + 0;
+}
 module.exports = {
   stringCal
-}
+};
