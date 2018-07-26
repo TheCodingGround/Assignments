9051357ca7da5f545098e2d4f2bc2c75ece1a335
Theo Bohnen <tbohnen.jnr@gmail.com>
2018-07-26 14:32:01 +0200

    Add code to get cookie by name

--- cookies/script.js
+++ cookies/script.js
@@ -1,6 +1,17 @@
 window.addEventListener("load", function(event) {
   console.log("loaded");
   var div = document.createElement("div");
-  div.innerHTML = document.cookie; //NOTE: This returns all cookies and you will need to split to get the right one
+
+  var cookie = getCookie("test")
+
+  div.innerHTML = cookie; //NOTE: This returns all cookies and you will need to split to get the right one
+
   document.body.appendChild(div);
 });
+
+//https://stackoverflow.com/questions/10730362/get-cookie-by-name
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
